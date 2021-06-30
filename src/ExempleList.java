3af2798b3754cf8b6111d60adc959d190a1010c1
Re04nan <renan04_marques@hotmail.com>
2021-06-30 15:47:35 -0300

    practing list

--- src/ExempleList.java
+++ src/ExempleList.java
@@ -1,16 +1,32 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class ExempleList {
 
     public static void main(String[] args){
 
-        List<String> wizards = new ArrayList<>(Arrays.asList("Harry Potter", "Hermione Granger"));
+        List<String> wizards = new ArrayList<>(Arrays.asList("Hermione Granger", "Harry Potter"));
         System.out.println(wizards);
         wizards.add("Ronald Weasley");
         System.out.println(wizards);
         System.out.println(wizards.contains("Ronald Weasley"));
         System.out.println(wizards.indexOf("Harry Potter"));
+
+        Collections.sort(wizards);
+
+        for (String listNames: wizards){
+            System.out.println(listNames);
+        }
+        Iterator<String> listTwo = wizards.iterator();
+        while (listTwo.hasNext()){
+            System.out.println(listTwo.next());
+        }
+
+        System.out.println(wizards.contains(3));
+
+        System.out.println(wizards.contains("Hermione Granger"));
+        System.out.println(wizards.isEmpty());
+        wizards.clear();
+        System.out.println(wizards.isEmpty());
+
     }
 }
